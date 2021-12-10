10fabd4fbe8699a5b70875df771b57dfe866df09
dayandnightlyn <89612896+dayandnightlyn@users.noreply.github.com>
2021-12-10 10:20:43 -0600

    Encapsulation
    Added getters, setters, and access modifiers to create encapsulation.

--- Main.java
+++ Main.java
@@ -6,8 +6,22 @@ public class Main {
 		
 		Tree mapleTree = new Tree(90, 30, TreeType.MAPLE);
 		
+		mapleTree.grow();
 		oakTree.announceTallTree();
-
+		mapleTree.announceTallTree();
+		Tree.announceTree();
+		
+		System.out.println(oakTree.getTreeType());
+		System.out.println(oakTree.getHeightFt());
+		System.out.println(oakTree.getDiameterInch());
+		
+		
+		oakTree.setHeightFt(150);
+		oakTree.setDiameterInch(16);
+		
+		System.out.println(oakTree.getHeightFt());
+		System.out.println(oakTree.getDiameterInch());
+		
 	}
 
 }
--- Tree.java
+++ Tree.java
@@ -1,24 +1,48 @@
 import java.awt.Color;
 
 public class Tree {
-	double heightFt;
-	double diameterInch;
-	TreeType treeType;
-	static Color TRUNK_COLOR = new Color(102, 51, 0);
+	private double heightFt;
+	private double diameterInch;
+	private TreeType treeType;
+	protected static Color TRUNK_COLOR = new Color(102, 51, 0);
 	
-	Tree(double heightFt, double diameterInch, TreeType treeType) {
+	public Tree(double heightFt, double diameterInch, TreeType treeType) {
 		this.heightFt = heightFt;
 		this.diameterInch = diameterInch;
 		this.treeType = treeType;
 	}
 	
-	void grow() {
+	public void grow() {
 		this.heightFt = this.heightFt + 10;
 		this.diameterInch = this.diameterInch + 1;
 	}
+	
+	public double getHeightFt() {
+		return heightFt;
+	}
+	
+	public void setHeightFt(double heightFt) {
+		this.heightFt = heightFt;
+	}
+	
+	public double getDiameterInch() {
+		return diameterInch;
+	}
+	
+	public void setDiameterInch(double diameterInch) {
+		this.diameterInch = diameterInch;
+	}
+	
+	public TreeType getTreeType() {
+		return treeType;
+	}
+	
+	public static void announceTree() {
+		System.out.println("Look at that " + TRUNK_COLOR + " tree!");
+	}
 
-	void announceTallTree() {
-		if(this.heightFt > 100) {
+	public void announceTallTree() {
+		if(this.heightFt >= 100) {
 			System.out.println("That's a tall " + this.treeType + " tree.");
 		}
 		
